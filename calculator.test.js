2b81c491137d18bf4f79dd1ef772c872b7763967
thaovo29 <thaovo2962002@gmail.com>
2023-10-15 17:04:02 +0700

    inital

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const calculator = require('./calculator')
 test('string with a single number should result in the number itself', () => {
     expect(calculator.add('1')).toBe(1);
   });
+
+test('add 1 + 2', () => {
+    expect(calculator.add('1,2')).toBe(3);
+})
